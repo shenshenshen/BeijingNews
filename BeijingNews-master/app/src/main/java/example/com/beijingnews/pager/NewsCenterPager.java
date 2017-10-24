9105bf682631f65d1ad6272831d0d9869911c7c0
jiezai <314337584@qq.com>
2017-10-24 22:06:38 +0800

    Gson解析

--- BeijingNews-master/app/src/main/java/example/com/beijingnews/pager/NewsCenterPager.java
+++ BeijingNews-master/app/src/main/java/example/com/beijingnews/pager/NewsCenterPager.java
@@ -2,10 +2,19 @@ package example.com.beijingnews.pager;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.support.constraint.solver.Goal;
 import android.view.Gravity;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+
+import org.xutils.common.Callback;
+import org.xutils.http.RequestParams;
+import org.xutils.x;
+
 import example.com.beijingnews.base.BasePager;
+import example.com.beijingnews.domain.NewsCenterPagerBean;
+import example.com.beijingnews.utiles.Constants;
 import example.com.beijingnews.utiles.LogUtil;
 
 /**
@@ -33,5 +42,48 @@ public class NewsCenterPager extends BasePager {
         fl_content.addView(textView);
         //4.绑定数据
         textView.setText("新闻中心内容");
+
+        getDataFromNet();
+    }
+
+    //使用xUtils3联网请求数据
+    public void getDataFromNet() {
+        RequestParams params = new RequestParams(Constants.NEWSCENTER_PAGER_URL);
+        x.http().get(params, new Callback.CommonCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                LogUtil.e("使用xUtils3联网请求成功=="+result);
+
+                processData(result);
+                //设置适配器
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+                LogUtil.e("使用xUtils3联网请求失败=="+ex.getMessage());
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+                LogUtil.e("使用xUtils3-onCancelled=="+cex.getMessage());
+            }
+
+            @Override
+            public void onFinished() {
+                LogUtil.e("使用xUtils3-onFinished");
+            }
+        });
+    }
+
+    //解析json数据和显示数据
+    private void processData(String json) {
+        NewsCenterPagerBean bean = parsedJson(json);
+        String title = bean.getData().get(0).getChildren().get(1).getTitle();
+        LogUtil.e("使用gson解析json数据成功-title=="+title);
+    }
+
+    //解析json数据
+    private NewsCenterPagerBean parsedJson(String json) {
+        return new Gson().fromJson(json,NewsCenterPagerBean.class);
     }
 }
